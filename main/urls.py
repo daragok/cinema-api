9ff16ab8d51a89fcf8ffe37b6a90f30b864649e5
Sergei <graser777@gmail.com>
2019-07-14 16:10:21 +0300

    remove duplicate /api/ from URLs

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from rest_framework.routers import DefaultRouter
 from main import views
 
 router = DefaultRouter()
-router.register(r'api/accounts', views.UserView, basename='accounts')
+router.register('accounts', views.UserView, basename='accounts')
 urlpatterns = router.urls
