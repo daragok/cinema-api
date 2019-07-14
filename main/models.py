2f7cf5346d54e9aa5e782fbf04613c09738f6f9f
Sergei <graser777@gmail.com>
2019-07-14 19:56:07 +0300

    add Theater Room model and migration that populates the table with 2 rooms

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class TheaterRoom(models.Model):
+    """ Theater room has rectangular shape and is defined by rows and seats count """
+    name = models.CharField(max_length=20)
+    rows_count = models.IntegerField()
+    seats_per_row_count = models.IntegerField()
